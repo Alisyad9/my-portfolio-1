c699f87e92be1482409ca1afd7dfde9ecea22806
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-02-17 00:49:51 +0000

    Hero: landing image styling

---
+++ src/Hero.js
@@ -0,0 +1,11 @@
+import './Nav.css';
+
+function Hero(props) {
+  const heroBackground = {
+    backGroundImage: './image/red.jpg',
+    height: '200px',
+  };
+  return <header style={heroBackground}>hello</header>;
+}
+
+export default Hero;
