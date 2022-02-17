68fb50a4ee02b3e6e010787fa4d1c1784f02c7e8
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-02-17 00:50:34 +0000

    imported components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Nav from './Nav';
+import Hero from './Hero';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
